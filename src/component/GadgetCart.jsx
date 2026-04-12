31837e8b46ea70ec8389acd85912bb24b33814a6
umayer10-AI <mdumayer10@gmail.com>
2026-04-12 21:03:38 +0600

    add

--- src/component/GadgetCart.jsx
+++ src/component/GadgetCart.jsx
@@ -8,7 +8,7 @@ const GadgetCart = ({p}) => {
             <Image width={100} height={100} src={p.product_image} alt='logo' className='rounded-2xl object-cover w-full h-40'></Image>
             <h2 className='text-xl font-bold'>{p.product_title}</h2>
             <h2 className='text-gray-400'>Price: {p.price}k</h2>
-            <Link href={`/${id}`} className='btn btn-primary btn-outline rounded-full'>View Details</Link>
+            <Link href={`/${p.id}`} className='btn btn-primary btn-outline rounded-full'>View Details</Link>
         </div>
     );
 };
